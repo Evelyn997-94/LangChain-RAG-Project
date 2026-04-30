eb70cfe5f3d128d7755bfd751082f4184b97f498
Evelyn Wu <wuyi23@mails.tsinghua.edu.cn>
2026-04-30 22:37:25 +0800

    Delete 02LangChain访问阿里云通义千问大模型.py

--- 02LangChain访问阿里云通义千问大模型.py
+++
@@ -1,8 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-model=ChatOpenAI(model="GLM-4-Air",
-                 base_url="https://llmapi.paratera.com")
-
-
-res = model.invoke("你是谁呀，能做什么？")
-print(res)
